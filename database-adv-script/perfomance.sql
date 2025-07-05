4966ae0af12cf20a428b5189adb12291bf21eefd
Assefa Endale <76453266+Asecoding@users.noreply.github.com>
2025-07-05 12:16:48 +0300

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -6,7 +6,6 @@ SELECT
     b.status,
     u.user_id,
     u.name AS user_name,
-    u.email,
     p.property_id,
     p.name AS property_name,
     p.location,
@@ -17,4 +16,9 @@ FROM Booking b
 JOIN User u ON b.user_id = u.user_id
 JOIN Property p ON b.property_id = p.property_id
 JOIN Payment py ON b.booking_id = py.booking_id
+WHERE b.status = 'confirmed'
+  AND b.booking_date >= CURRENT_DATE - INTERVAL '30 days'
 ORDER BY b.booking_date DESC;
+
+EXPLAIN ANALYZE
+SELECT ...
